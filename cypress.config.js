8ba7ab48055151ab24f835599f02578bc7f69b6c
MaharjanNaffi <nafisha078@academiacollege.edu.np>
2025-11-30 14:46:44 +0545

    added mochawesome

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,14 @@ const { defineConfig } = require('cypress');
 require('dotenv').config(); // load .env file
 
 module.exports = defineConfig({
+  reporter: 'mochawesome', // set mochawesome as reporter
+  reporterOptions: {
+    reportDir: 'cypress/reports', // folder where reports will be saved
+    overwrite: false,            // do not overwrite previous reports
+    html: true,                  // generate HTML report
+    json: true,                  // generate JSON report
+    timestamp: 'mmddyyyy_HHMMss' // optional: add timestamp to report filename
+  },
   e2e: {
     baseUrl: process.env.CYPRESS_BASE_URL,
     env: {
@@ -10,6 +18,10 @@ module.exports = defineConfig({
     },
     setupNodeEvents(on, config) {
       console.log('Loaded ENV:', config.env);
+
+      // optional: merge mochawesome options dynamically
+      require('cypress-mochawesome-reporter/plugin')(on);
+
       return config;
     },
   },
